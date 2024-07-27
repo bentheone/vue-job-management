02c521511a9273c70b07689247b29798dec468eb
bentheone <rben5838@gmail.com>
2024-07-27 20:40:39 +0200

    Made Home, Jobs and not found pages all made of components. Also set up vue router

---
+++ src/components/JobListings.vue
@@ -0,0 +1,38 @@
+<script setup>
+ import jobData from '@/jobs.json'
+ import {ref, defineProps} from 'vue'
+ import JobListing from '@/components/JobListing.vue'
+ import {RouterLink} from 'vue-router'
+
+ defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false
+    }
+ })
+
+ const jobs = ref(jobData)
+</script>
+
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h1 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Jobs
+            </h1>
+            <div class="grid grid-cols md:grid-cols-3 gap-6">
+                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" /> 
+            </div>
+        </div>
+    </section>
+    <div>
+    </div>
+    
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <RouterLink
+        to="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</RouterLink>
+    </section>
+</template>
